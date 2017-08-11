060a2d4f72dc46e6c7297d8e09137aebc14783d8
Bryan Perris <bperris@geisel-software.com>
2017-08-11 12:43:15 -0400

    sync script fix

--- .vscode/sync.sh
+++ .vscode/sync.sh
@@ -2,4 +2,4 @@
 ./coffeebuild.sh
 cd automata
 vagrant rsync
-vagrant ssh -c "export PATH=$PATH:/usr/local/bin; sudo pkill supervisord; sudo supervisord -c /etc/supervisord.conf; echo done"
+vagrant ssh -c "sudo su -c 'export PATH=$PATH:/usr/local/bin; pkill supervisord; supervisord -c /etc/supervisord.conf'; echo done"
